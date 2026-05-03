8e1539d122871ba9e9065449aaa35988d10257eb
noaeliyahou <eliyahon@post.bgu.ac.il>
2026-05-03 12:49:48 +0300

    Added GeneralCheckingFunctions

--- src/test/GeneralCheckingFunctions.java
+++ src/test/GeneralCheckingFunctions.java
@@ -1,4 +1,8 @@
 package test;
 
 public class GeneralCheckingFunctions {
+    public static String getGithubLink(){
+        String githubLink = "https://github.com/<noaeliyahou>/ATP-Project";
+        return githubLink;
+    }
 }
